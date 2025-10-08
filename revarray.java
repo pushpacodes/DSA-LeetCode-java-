55e4da471f89d84f3dbe9ccf72f973e407550f23
kalva pushpalatha <kalvapushpaaiml50@gmail.com>
2025-10-08 08:36:24 +0530

    Add files via upload

---
+++ revarray.java
@@ -0,0 +1,27 @@
+public class revarray {
+    public static void reverse(int numbers[]){
+        int first = 0;
+        int last = numbers.length-1;
+        while(first<last){
+            int temp = numbers[first];
+            numbers[first]=numbers[last];
+            numbers[last]=temp;
+            first++;
+            last--;
+        }
+        System.out.print("reversed array is:");
+        for(int i =0;i<numbers.length;i++){
+            System.out.print(numbers[i]+" ");
+        }
+    }
+    public static void main(String args[]){
+        int numbers[]= {2,4,6,8,10,12};
+        System.out.print("original array is:");
+        for(int i =0;i<numbers.length;i++){
+            System.out.print(numbers[i]+" ");
+        }   
+        System.out.println();
+        reverse(numbers); 
+    }
+}
+
