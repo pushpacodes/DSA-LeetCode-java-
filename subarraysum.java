5237c88a74466c19a9ce9b57b20dfaab38d70f2c
kalva pushpalatha <kalvapushpaaiml50@gmail.com>
2025-10-08 19:34:44 +0530

    Update subarraysum.java

--- subarraysum.java
+++ subarraysum.java
@@ -1,5 +1,8 @@
+import java.util.*;
 public class subarraysum {
     public static void pairs(int numbers[]) {
+        int maxsum = Integer.MIN_VALUE; // initialize maxsum to the smallest integer value
+        int minsum = Integer.MAX_VALUE; // initialize minsum to the largest integer value
         for (int i = 0; i < numbers.length; i++) {
             int start = i;
             for (int j = i; j < numbers.length; j++) { // include i itself
@@ -11,12 +14,20 @@ public class subarraysum {
                     System.out.print(numbers[k] + " ");
                     sum += numbers[k];
                 }
+                if (sum > maxsum){
+                    maxsum=sum;
+                }
+                if (sum<minsum){
+                    minsum=sum;
+                }
 
                 // print subarray sum
                 System.out.println(" Its Sum = " + sum);
             }
             System.out.println();
         }
+        System.out.println("Maximum Subarray Sum = " + maxsum);
+        System.out.println("Minimum Subarray Sum = " + minsum);
     }
 
     public static void main(String args[]) {
